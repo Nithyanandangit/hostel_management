c162c7d126c208408395d88a65c139fa63452ed7
Nithyanandangit <nithikumaran10@gmail.com>
2024-07-04 12:37:45 +0530

    first commit

---
+++ check_availability.php
@@ -0,0 +1,72 @@
+<?php
+require_once("includes/config.php");
+if(!empty($_POST["emailid"])) {
+	$email= $_POST["emailid"];
+	if (filter_var($email, FILTER_VALIDATE_EMAIL)===false) {
+
+		echo "error : You did not enter a valid email.";
+	}
+	else {
+		$result ="SELECT count(*) FROM userRegistration WHERE email=?";
+		$stmt = $mysqli->prepare($result);
+		$stmt->bind_param('s',$email);
+		$stmt->execute();
+$stmt->bind_result($count);
+$stmt->fetch();
+$stmt->close();
+if($count>0)
+{
+echo "<span style='color:red'> Email already exist .</span>";
+}
+else{
+	echo "<span style='color:green'> Email available for registration.</span>";
+}
+}
+}
+
+if(!empty($_POST["password"])) {
+	$pass= $_POST["password"];
+$uppercase = preg_match('@[A-Z]@', $pass);
+$lowercase = preg_match('@[a-z]@', $pass);
+$number    = preg_match('@[0-9]@', $pass);
+$specialChars = preg_match('@[^\w]@', $pass);
+
+if(!$uppercase || !$lowercase || !$number || !$specialChars || strlen($pass) < 8) {
+    echo "<span style='color:red'> Password should be at least 8 characters in length and should include at least one upper case letter, one number, and one special character.";
+}else{
+    echo "<span style='color:green'> Strong password.";
+}
+}
+
+if(!empty($_POST["oldpassword"])) 
+{
+$pass=$_POST["oldpassword"];
+$result ="SELECT password FROM userregistration WHERE password=?";
+$stmt = $mysqli->prepare($result);
+$stmt->bind_param('s',$pass);
+$stmt->execute();
+$stmt -> bind_result($result);
+$stmt -> fetch();
+$opass=$result;
+if($opass==$pass) 
+echo "<span style='color:green'> Password  matched .</span>";
+else echo "<span style='color:red'> Password Not matched</span>";
+}
+
+
+if(!empty($_POST["roomno"])) 
+{
+$roomno=$_POST["roomno"];
+$result ="SELECT count(*) FROM registration WHERE roomno=?";
+$stmt = $mysqli->prepare($result);
+$stmt->bind_param('i',$roomno);
+$stmt->execute();
+$stmt->bind_result($count);
+$stmt->fetch();
+$stmt->close();
+if($count>0)
+echo "<span style='color:red'>$count. Seats already full.</span>";
+else
+	echo "<span style='color:red'>All Seats are Available</span>";
+}
+?>
